d13d006175608839d81db9df30383e8df02fa325
Rex Ye <rxye1991@gmail.com>
2020-03-13 16:58:39 -0400

    testingDeploymeng

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -173,7 +173,7 @@ class FixedNavbarExample extends React.Component {
                     <MDBCollapse isOpen = { this.state.collapse } navbar>
                       <MDBNavbarNav left>
                         <MDBNavItem active>
-                            <MDBNavLink exact to="/">Home</MDBNavLink>
+                            <MDBNavLink exact to="/">ffffff</MDBNavLink>
                         </MDBNavItem>
                         <MDBNavItem active>
                         <MDBNavLink exact onClick ={this.handleLoans} to="/">Loans</MDBNavLink>
