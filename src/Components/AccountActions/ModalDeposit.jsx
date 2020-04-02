837a034aad8f5437645e7550c3cdc60dfd0407b4
Rex Ye <rxye1991@gmail.com>
2020-04-02 19:51:06 -0400

    clearDepositAmountUponSubmit

--- src/Components/AccountActions/ModalDeposit.jsx
+++ src/Components/AccountActions/ModalDeposit.jsx
@@ -1,40 +1,3 @@
-// import React from 'react'
-// import { Button, Header, Image, Modal } from 'semantic-ui-react'
-// import {connect} from 'react-redux'
-
-// const submitDeposit = (e) => {
-//     e.preventDefault()
-//     console.log(e.target.amount.value)
-// }
-// **** const 
-
-// const mstp = (appState) => {
-
-//   console.log(appState)
-// }
-
-// const ModalDeposit = () => (
-//   <Modal trigger={<Button color='black'>Deposit</Button>} centered={false}>
-//     <Modal.Header>Select a Photo</Modal.Header>
-//     <Modal.Content image>
-//       {/* <Image wrapped size='medium' src='https://react.semantic-ui.com/images/avatar/large/rachel.png' /> */}
-//       <Modal.Description>
-//         <Header>Make a deposit</Header>
-//         <form onSubmit = {submitDeposit}>
-//           <label htmlFor="">Deposit Amount</label>
-//           <input type="number" name = 'amount' />
-//           <br/>
-//           <input type="submit"/>
-//         </form>
-//         <p>Is it okay to use this photo?</p>
-//       </Modal.Description>
-//     </Modal.Content>
-//   </Modal>
-// )
-
-
-// export default connect(mstp)(ModalDeposit)
-
 import React, { Component } from 'react'
 import { Button, Header, Image, Modal } from 'semantic-ui-react'
 import {connect} from 'react-redux'
@@ -42,7 +5,7 @@ import {onlineCheckingDeposit} from '../Redux/actions'
 import swal from 'sweetalert';
 import {withRouter} from 'react-router-dom'
 export class ModalDeposit extends Component {
-  
+
       submitDeposit = (e) => {
           e.preventDefault()
           const amount = e.target.amount.value
@@ -52,9 +15,7 @@ export class ModalDeposit extends Component {
       if(amount < 20){
           swal('Please make a minimum deposit of $20')
       }
-      
       else if (amount > 5000){
-
           swal(
             'Alert!!',
             'Deposit Amount is too large, please visit one of our five thousands branches.',
@@ -65,7 +26,8 @@ export class ModalDeposit extends Component {
         swal(``,
         "Deposit Made",
         "success");
-      
+        e.target.amount.value = ''
+    
           fetch(`https://flatironbankapi.herokuapp.com/checkings/deposit/${checkingId}`, {
               method:'PATCH',
               headers: {
