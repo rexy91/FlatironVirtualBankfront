c4cb40df5559a7d1e2fd1361fdd6a8598ce3ab71
Rex Ye <rxye1991@gmail.com>
2020-04-13 15:35:51 -0400

    LogingToggleChineseLanguageFinished

--- src/Components/AccountActions/MDBLogin.jsx
+++ src/Components/AccountActions/MDBLogin.jsx
@@ -43,30 +43,33 @@ export class MDBLogin extends Component {
       })
   }
 
-renderLogin =() => {
-  console.log(this.props)
+renderLoginChinese = () => {
   if(localStorage.length === 0){
     {return <div id='login-form'>
-    {/* <MDBContainer>
-      <MDBRow>
-        <MDBCol md="6">
-          <form onSubmit ={this.handleLoginSubmit}>
-            <p className="h5 text-center mb-4">Sign in</p>
-            <div className="grey-text">
-              <p id='username-instructions' >Username length: between 5-10</p>
-              <MDBInput id = 'username-input' name = 'username' icon="user-alt" group type="username" 
-               onChange = {this.handleChange} value = {this.state.username} />
-               <p className = 'signin-instructions' >Password length: between 7-20</p>
-              <MDBInput name = 'password' icon="lock" group type="password"
-               onChange = {this.handleChange} value = {this.state.password} />
-            </div>
-            <div className="text-center">
-              <MDBBtn id='login-button' type = 'submit'>Login</MDBBtn>
-            </div>
-          </form>
-        </MDBCol>
-      </MDBRow>
-    </MDBContainer> */}
+              <Form id='login-form-style'  onSubmit ={this.handleLoginSubmit}> 
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>用戶名</Form.Label>
+              <Form.Control placeholder="Enter email" name = 'username' 
+                onChange = {this.handleChange} value = {this.state.username} />
+            </Form.Group>
+
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>密码</Form.Label>
+              <Form.Control type="password" placeholder="Password" name = 'password' 
+                onChange = {this.handleChange} value = {this.state.password} />
+            </Form.Group>
+            {/* <Button variant="white" type="submit" style={{opacity: '0.6'}}>
+              Submit
+            </Button> */}
+            <MDBBtn type = 'submit' id='register-button' >登入</MDBBtn>
+          </Form>
+    </div>}
+}}
+
+renderLoginEnglish =() => {
+  console.log('here')
+  if(localStorage.length === 0){
+    {return <div id='login-form'>
               <Form id='login-form-style'  onSubmit ={this.handleLoginSubmit}> 
             <Form.Group controlId="formBasicEmail">
               <Form.Label>Username</Form.Label>
@@ -91,10 +94,9 @@ else{
 }
 }
     render() {
-      // console.log(this.state)
         return (
             <>
-              {this.renderLogin()}
+              {this.props.language === 'Chinese'? this.renderLoginChinese() : this.renderLoginEnglish()}
             </>
         )
     }
